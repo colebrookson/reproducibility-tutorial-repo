6397c74d0e820cedaa13535ae021e98e1c4d3661
Cole Brookson <cole.brookson@gmail.com>
2021-05-20 14:03:14 -0600

    separate out code into files

---
+++ __main__.R
@@ -0,0 +1,14 @@
+########## 
+##########
+# This main file calls and runs all subsequent R files in this analysis
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-05-20
+##########
+##########
+
+# set-up =======================================================================
+
+library(here)
+
---
+++ src/visualization.R
@@ -0,0 +1,32 @@
+########## 
+##########
+# This main file calls and runs all subsequent R files in this analysis
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2021-05-20
+##########
+##########
+
+# set-up =======================================================================
+
+library(tidyverse)
+library(here)
+
+setosa_subset = read_csv(here('./output/setosa_subset_data.csv'))
+new_data = read_csv(here('./output/new_data_for_prediction.csv'))
+
+# make and save a simple plot
+simple_fig = ggplot(data = setosa_subset) +
+  geom_point(aes(x = Sepal.Length, y = Petal.Width)) +
+  labs(x = "sepal length", y = "petal width") +
+  theme_bw()
+ggsave(here('./figs/simple_iris_fig.png'), simple_fig)
+
+# plot prediction
+prediction_plot = ggplot(data = new_data) +
+  geom_line(aes(x = Petal.Length, y = prediction),
+            size = 2, colour = 'red') +
+  labs(x = "petal length", y = "sepal length prediction") +
+  theme_bw()
+ggsave(here('./figs/prediction_plot_simple.png'), prediction_plot)
