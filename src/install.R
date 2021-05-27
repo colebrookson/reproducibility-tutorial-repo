5b8c9c36678817f7738392332a66bb55ab1a21d7
colebrookson <cole.brookson@gmail.com>
2021-05-27 10:17:16 -0600

    add install script

---
+++ src/install.R
@@ -0,0 +1,11 @@
+###############
+###############
+# This file installs the requisite packages to work in a binder environment 
+###############
+###############
+# AUTHOR: Cole B. Brookson
+# INSTANIATION DATE: 2021-05-13
+###############
+###############
+
+install.packages(c("here","tidyverse"))
