a7b352852fb0fd37c10c5ba97961ee5fb2e6cfa1
colebrookson <cole.brookson@gmail.com>
2021-06-10 01:37:50 -0600

    put all fixed components on the repo

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM r-base:4.1.0
 WORKDIR /main
 RUN R -e "options(repos = \
   list(CRAN = 'http://mran.revolutionanalytics.com/snapshot/2021-06-09/')); \
-  install.packages('readr', 'ggplot2')"
+  install.packages('readr'); install.packages('ggplot2')"
 CMD ["Rscript", "__main__.R"]
 
--- __main__.R
+++ __main__.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ########## 
 ##########
 # This main file calls and runs all subsequent R files in this analysis
@@ -24,66 +23,12 @@ source('./src/data-wrangling.R')
 # This script reads in the appriate csvs and fits a shitty GLM, then makes a 
 # model prediction on in and out of sample data
 
-rm(list = ls())
 source('./src/model-fitting.R')
 
 # model visualization ==========================================================
 
 # this script reads in two dataframes and creates and saves simple plots 
 
-rm(list = ls())
 source('./src/visualization.R')
 
 
-=======
-########## 
-##########
-# This main file calls and runs all subsequent R files in this analysis
-##########
-##########
-# AUTHOR: Cole B. Brookson
-# DATE OF CREATION: 2021-05-20
-##########
-##########
-
-# set-up =======================================================================
-library(here)
-
-# data wrangling ===============================================================
-
-# This script reads in iris data and performs some simple data wrangling, then 
-# writes out the files needed for model fitting 
-
-source(here('./src/data-wrangling.R'))
-
-# model fitting ================================================================
-
-# This script reads in the appriate csvs and fits a shitty GLM, then makes a 
-# model prediction on in and out of sample data
-
-rm(list = ls())
-source(here('./src/model-fitting.R'))
-
-# model visualization ==========================================================
-
-# this script reads in two dataframes and creates and saves simple plots 
-
-rm(list = ls())
-source(here('./src/visualization.R'))
-
-
-x = 2
-
-if(x == 2) {
-  exit("You idiot, x is 2")
-}
-
-print(x)
-
-
-
-
-
-
-
->>>>>>> cfe65c58be6ff7ded01b99ef868f1a0af08c38ec
